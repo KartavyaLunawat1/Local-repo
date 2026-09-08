15d72fb05c67931cb9c75b1b070760e15d97dc86
Kartavya Lunawat <kartavyalunawat800@gmail.com>
2026-09-08 19:41:23 +0530

    New 2nd C file

---
+++ code2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main() {
+    int x,y,sum;
+    printf("Enter value of X: ");
+    scanf("%d", &x);
+     printf("Enter value of Y: ");
+    scanf("%d", &y);
+    sum = x+y;
+    printf("Sum of X & Y = %d", sum);
+
+    return 0;
+}
